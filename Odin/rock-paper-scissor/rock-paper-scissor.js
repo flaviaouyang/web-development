f50bc028cfb4ee283cb0af7a732dee370982fa93
Flavia O <flavia.ouyang@mail.mcgill.ca>
2021-12-09 17:59:47 -0500

    updated console content to DOM

--- Odin/rock-paper-scissor/rock-paper-scissor.js
+++ Odin/rock-paper-scissor/rock-paper-scissor.js
@@ -6,20 +6,30 @@ const rock = document.querySelector("#btn-rock");
 const paper = document.querySelector("#btn-paper");
 const scissors = document.querySelector("#btn-scissors");
 
+const compMove = document.querySelector("#computer-move");
+const playMove = document.querySelector("#player-move");
+
+const compScoreDisplay = document.querySelector("#computer-score");
+const playScoreDisplay = document.querySelector("#player-score");
+
 rock.addEventListener("click", function () {
   playRound("Rock");
+  playMove.textContent = "Rock";
 });
 paper.addEventListener("click", function () {
   playRound("Paper");
+  playMove.textContent = "Paper";
 });
 scissors.addEventListener("click", function () {
   playRound("Scissors");
+  playMove.textContent = "Scissors";
 });
 
 function computerPlay() {
   // put rock paper scissors into an array
   let gesture = ["Rock", "Paper", "Scissors"];
   let which = Math.floor(Math.random() * 3);
+  compMove.textContent = gesture[which];
   return gesture[which];
 }
 
@@ -34,34 +44,48 @@ function playRound(player) {
 
   if (computer === player) {
     console.log("Tie");
+    compScoreDisplay.textContent = comScore;
+    playScoreDisplay.textContent = playScore;
   }
   if (computer === "Rock" && player === "Scissors") {
     comScore += 1;
+    compScoreDisplay.textContent = comScore;
+    playScoreDisplay.textContent = playScore;
     console.log("Computer won.");
   }
 
   if (computer === "Paper" && player === "Rock") {
     comScore += 1;
+    compScoreDisplay.textContent = comScore;
+    playScoreDisplay.textContent = playScore;
     console.log("Computer won.");
   }
 
   if (computer === "Scissors" && player === "Paper") {
     comScore += 1;
+    compScoreDisplay.textContent = comScore;
+    playScoreDisplay.textContent = playScore;
     console.log("Computer won.");
   }
 
   if (computer === "Rock" && player === "Paper") {
     playScore += 1;
+    compScoreDisplay.textContent = comScore;
+    playScoreDisplay.textContent = playScore;
     console.log("Player won.");
   }
 
   if (computer === "Paper" && player === "Scissors") {
     playScore += 1;
+    compScoreDisplay.textContent = comScore;
+    playScoreDisplay.textContent = playScore;
     console.log("Player won.");
   }
 
   if (computer === "Scissors" && player === "Rock") {
     playScore += 1;
+    compScoreDisplay.textContent = comScore;
+    playScoreDisplay.textContent = playScore;
     console.log("Player won.");
   }
 }
